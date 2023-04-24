b363edc0c998bc58d280e1381f04a5fff9bc3b67
jesusd63 <89026878+jesusd63@users.noreply.github.com>
2023-04-24 11:09:04 -0600

    Se quitó el Modal por error de id

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/Home"
 
 function App() {
   return (
