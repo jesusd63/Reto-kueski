4ecfebc446909dc548b76282c1850186e07d55d7
DelRosal <ana.delrosal01@gmail.com>
2023-04-24 12:03:53 -0600

    index from server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const mysql = require("mysql2");
 const cors = require("cors");
 
 const path= require("path")
-app.use( express.static( path.resolve( __dirname, '../client/src')));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,6 +17,17 @@ const db = mysql.createPool({
     database: "kueski_data",
 });
 
+app.use( express.static( path.resolve( __dirname, '../client/src')));
+
+
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, '../client/build', 'index.html'));
+    });
+
+    
+app.listen(3001, () => {
+    console.log("Server listening on port 3001");
+})
 
 app.get("/menu", (req, res) => {
     const sqlGet = "SELECT * FROM users";
@@ -25,20 +35,6 @@ app.get("/menu", (req, res) => {
         res.send(result);
     });
 });
-
-app.get("/", (req, res) => {
-    //const sqlInsert = 
-    //    "INSERT INTO users (user_id, email, name, last_name, second_last_name, curp, rfc) VALUES (2, 'rafaelbelloni@gmail.com', 'Rafael', 'Belloni', 'Rocha', 'JDKS984304JDNEF01', 'JDKS984304342')";
-    //db.query(sqlInsert,(error, result) => {
-    //    console.log("error", error);
-    //    console.log("result", result);
-    //    res.send("hello world");
-    //});
-});
-
-app.listen(3001, () => {
-    console.log("Server listening on port 3001");
-})
 
 db.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
   if (err) throw err
