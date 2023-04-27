5310854e450db045fd56255d2042498dc88aa2fa
Ingrid González <glez.ingrid23@gmail.com>
2023-04-27 10:43:17 -0600

    cancel page

--- client/src/pages/Cancel.js
+++ client/src/pages/Cancel.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Cancel.css'
 import {Link} from 'react-router-dom'
+import { AiFillCloseCircle } from "react-icons/ai";
 
-export default function Cancel() {
+export default function Cancel(props) {
 	return (
 		
 		<div className='cancel'>
 			<span className='head'>Cancelación - Rafael Nicolas Belloni Rocha</span>
+			<div>
+				<AiFillCloseCircle onClick={props.onCancel}  className='close'/>
+			</div>
+
 			<span className='info'>Por este medio y según los alineamientos de los derechos ARCO con los que cuenta, el usuario Rafael Nicolas Belloni Rocha solicita la cancelación en el uso de sus datos personales.</span>
 			<span className='razon'>Razón del Usuario:</span>
 			<div className='input'>
